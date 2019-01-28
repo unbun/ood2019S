08884a04e50192b870f86133003c26f4fbd83c60
unbun <uwh.547@gmail.com>
2019-01-27 23:37:35 -0500

    junit is running

--- lectures/test/unbun/cs3500/lectures/basicInheritance/durations/DurationTests.java
+++ lectures/test/unbun/cs3500/lectures/basicInheritance/durations/DurationTests.java
@@ -1,7 +1,13 @@
 package unbun.cs3500.lectures.basicInheritance.durations;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class DurationTests {
 
+  @Test
+  public void dummy(){
+    assertEquals(1,1);
+  }
 }
